b63b73bf85c4abd01003e653aaf8c8b64ecaea03
JiayiJoyce <jiayijoyce@163.com>
2023-10-20 11:50:30 +0100

    Update firstscript.R

--- firstscript.R
+++ firstscript.R
@@ -1,8 +1,30 @@
 install.packages(tidyverse)
-5+3
-{Print(helloworld)}
-print(helloworld)
-print("helloworld")
+install.packages("pacman")
+pacman::p_load(
+  tidyverse, 
+  skimr,
+  janitor
+)
+dplyr::arrange(mtcars, mpg) 
+dplyr::arrange(mtcars, desc(mpg))
+
+df <- mtcars %>% rename (milepergalon = mpg) #store change in a differen dataframe called df.
+glimpse (df) #don't change the original dataframe
+# storing change in a new data frame called df
+
+#starwars %>%
+  #dplyr::filter(sex=="female") %>%
+  #arrange (desc(height))
+
+starwars %>%
+  dplyr::filter(height < 180 & height > 160) 
+
+msleep %>%
+  filter(order == "Primates", sleep_total > 10) %>%
+  select(name, sleep_rem, sleep_cycle, sleep_total) %>%
+  arrange(name) %>%
+  mutate(sleep_total_min = sleep_total * 60)
+
 
 
 
